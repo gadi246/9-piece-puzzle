4d0f4c5097bfde8526768a3d60b2e4aca5041e5f
gaditzkhori <Gt2017!!>
2018-01-22 13:37:41 +0200

    sdffsd

--- src/App/Components/ImageItem.js
+++ src/App/Components/ImageItem.js
@@ -3,7 +3,6 @@ import {Component} from '../../lib/component';
 class ImageItem extends Component {
 
   render(style = '') {
-    console.log('image itemghfghfgh');
     this.props.elm.style = this.props.data.css;
     this.props.elm.style.transform = style;
     this.props.elm.addEventListener('click', () => this.props.onClick(this.props.data, this.render.bind(this)));
